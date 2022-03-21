fc713f0063f8d898b25a52009a2773c363a21dc4
virtual-luna <maya.ellington@berkeley.edu>
2022-03-21 12:03:30 -0700

    pointed array values to number values on board

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 /*----- constants -----*/
 
-// const marbles = 48; //total number of marbles in game 
+//all pit/mancala values held in single array, start with 4 seeds in each pit
+const gameArray = [0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0];
+const startMsg = "CLICK TO START";
+const player1Msg = "Player 1's Turn!"
+const player2Msg = "Player 2's Turn!"
+
+//-----------------------------------------------------
+
+
+
 
 /*----- app's state (variables) -----*/
 
-let pits1;
-let pits2;
-let store1;
-let store2;
+let player1;
+let player2;
+
+let msgBtn = document.querySelector('#message-display');
+
+document.querySelector('.mancala-total-1').innerHTML = gameArray[0];
+document.querySelector('.pit-a').innerHTML = gameArray[1];
+document.querySelector('.pit-b').innerHTML = gameArray[2];
+document.querySelector('.pit-c').innerHTML = gameArray[3];
+document.querySelector('.pit-d').innerHTML = gameArray[4];
+document.querySelector('.pit-e').innerHTML = gameArray[5];
+document.querySelector('.pit-f').innerHTML = gameArray[6];
+document.querySelector('.pit-g').innerHTML = gameArray[7];
+document.querySelector('.pit-h').innerHTML = gameArray[8];
+document.querySelector('.pit-i').innerHTML = gameArray[9];
+document.querySelector('.pit-j').innerHTML = gameArray[10];
+document.querySelector('.pit-k').innerHTML = gameArray[11];
+document.querySelector('.pit-l').innerHTML = gameArray[12];
+document.querySelector('.mancala-total-2').innerHTML = gameArray[13];
+
+
+
+
+//-----------------------------------------------------
+
+
+
 
 /*----- cached element references -----*/
 
@@ -15,41 +47,55 @@ let store2;
 
 let pit = document.querySelector('.pit');
 
+msgBtn.innerHTML = startMsg;
+
 //changes initial message to indicate whose turn it is
-const startBtn = document.querySelector('#message-display');
-startBtn.addEventListener('click', startGame);
+msgBtn.addEventListener('click', startGame);
+
+
+
+//-----------------------------------------------------
+
+
 
 
 /*----- event listeners -----*/
 
-startBtn.addEventListener('click', init);
+msgBtn.addEventListener('click', init);
 init();
 
 pit.addEventListener('click', gameRound)
 
+
+
+
+//-----------------------------------------------------
+
+
+
+
 /*----- functions -----*/
+
+
+
+
 
 //render()  //in init function?
 
 //set up values in init function
 function init() {
-    // let pits1 = [4, 4, 4, 4, 4, 4];
-    // let pits2 = [4, 4, 4, 4, 4, 4];
-    let store1 = 0;
-    let store2 = 0;
-    
 }
 
 //when player clicks pit, all seeds in that pit get distributed around each consequtive pit
 function startGame(){
-    startBtn.innerHTML = "Player 1's Turn!"
+    msgBtn.innerHTML = player1Msg
 }
 
 // function handleClick() {
 // pits1[1] + 1
 // }
 
-//WORK THIS OUT
+//WORK THIS OUT?
 function gameRound(){
     console.log(pit.innerHTML)
 }
