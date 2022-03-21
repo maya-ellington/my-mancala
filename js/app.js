9524f13c2fd9a37b98f60bf17a89028b99b09700
virtual-luna <maya.ellington@berkeley.edu>
2022-03-20 18:20:28 -0700

    initial css set up complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 /*----- constants -----*/
+const marbles = 48; //total number of marbles in game
+
+
 /*----- app's state (variables) -----*/
+let pits1 = [4, 4, 4, 4, 4, 4]; 
+let pits2 = [4, 4, 4, 4, 4, 4]; 
+let store1 = 0;
+let store2 = 0;
+
+
+
 /*----- cached element references -----*/
+
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+
+    //render()
